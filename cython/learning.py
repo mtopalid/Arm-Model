8baccced32c672db2d8580346e02310dbdfb022a
mtopalid <meropi.topalidou@inria.fr>
2015-11-26 12:14:17 +0100

    Added  continuous learning and save for safe

--- cython/learning.py
+++ cython/learning.py
@@ -12,36 +12,42 @@ from trial import *
 from parameters import *
 import sys
 
-def learning_trials_single(task, ncues=2, trials=n_trials, learn=True, debugging=True,                                  debug_simulation=False,
+
+def learning_trials_single(task, ncues=2, trials=n_trials, learn=True, debugging=True, debug_simulation=False,
                            debugging_arm=False, debugging_arm_learning=False,
-                           duration=duration, folder=''):
+                           duration=duration, folder='', tr=0, save=False):
     if debug_simulation:
         steps = trials / 10
         print '  Starting   ',
 
-    for i in range(trials):
+    for i in range(tr, trials):
 
-        trial(task, ncues=ncues, trial_n=i, learn=learn, debugging=debugging,                               debugging_arm=debugging_arm, duration=duration)
+        trial(task, ncues=ncues, trial_n=i, learn=learn, debugging=debugging, debugging_arm=debugging_arm,
+              duration=duration)
 
-        f = folder + '/Cues.npy'
-        np.save(f, task.trials[i])
-        f = folder + '/Records.npy'
-        np.save(f, task.records[i])
-        f = folder + '/Trial_Number.npy'
-        np.save(f, i)
+        if save:
+            f = folder + '/Cues.npy'
+            np.save(f, task.trials[i])
+            f = folder + '/Records.npy'
+            np.save(f, task.records[i])
+            f = folder + '/Trial_Number.npy'
+            np.save(f, i)
+
+            if i % 1000 == 0:
+                f = folder + '/Cues1000s.npy'
+                np.save(f, task.trials[:i])
+                f = folder + '/Records1000s.npy'
+                np.save(f, task.records[:i])
+                f = folder + '/Trial_Number1000s.npy'
+                np.save(f, i)
 
         if debug_simulation:
             if i % steps == 0:
                 print '\b.',
                 sys.stdout.flush()
-
-        if i % 1000 == 0:
-            f = folder + '/Cues' + "%03d" % (i+1) + '.npy'
-            np.save(f, task.trials[:i])
-            f = folder + '/Records' + "%03d" % (i+1) + '.npy'
-            np.save(f, task.records[:i])
-            f = folder + '/Trial_Number' + "%03d" % (i+1) + '.npy'
-            np.save(f, i)
+    if save:
+        f = folder + '/RecordsALL.npy'
+        np.save(f, task.records[:i])
     if debug_simulation:
         print '   Done!'
     if debugging_arm_learning:
@@ -50,33 +56,36 @@ def learning_trials_single(task, ncues=2, trials=n_trials, learn=True, debugging
     return
 
 
-def learning_trials_continuous(task, ncues=2, trials=n_trials, learn=True, debugging=True,                                  debug_simulation=False, debugging_arm=False,                                                 debugging_arm_learning=False, folder="",duration=duration):
+def learning_trials_continuous(task, ncues=2, trials=n_trials, learn=True, debugging=True, debug_simulation=False,
+                               debugging_arm=False, debugging_arm_learning=False, folder="", duration=duration, save=False):
     if debug_simulation:
         steps = trials / 10
         print '  Starting   ',
 
     for i in range(trials):
 
-        trial_continuous(task, ncues=ncues, trial_n=i, learn=learn, debugging=debugging,                                debugging_arm=debugging_arm, duration=duration)
+        trial_continuous(task, ncues=ncues, trial_n=i, learn=learn, debugging=debugging, debugging_arm=debugging_arm,
+                         duration=duration)
 
-        f = folder + '/Cues.npy'
-        np.save(f, task.trials[i])
-        f = folder + '/Records.npy'
-        np.save(f, task.records[i])
-        f = folder + '/Trial_Number.npy'
-        np.save(f, i)
+        if save:
+            f = folder + '/Cues.npy'
+            np.save(f, task.trials[i])
+            f = folder + '/Records.npy'
+            np.save(f, task.records[i])
+            f = folder + '/Trial_Number.npy'
+            np.save(f, i)
+            if i % 1000 == 0:
+                f = folder + '/Cues' + "%03d" % (i + 1) + '.npy'
+                np.save(f, task.trials[:i])
+                f = folder + '/Records' + "%03d" % (i + 1) + '.npy'
+                np.save(f, task.records[:i])
+                f = folder + '/Trial_Number' + "%03d" % (i + 1) + '.npy'
+                np.save(f, i)
 
         if debug_simulation:
             if i % steps == 0:
                 print '\b.',
                 sys.stdout.flush()
-        if i % 1000 == 0:
-            f = folder + '/Cues' + "%03d" % (i+1) + '.npy'
-            np.save(f, task.trials[:i])
-            f = folder + '/Records' + "%03d" % (i+1) + '.npy'
-            np.save(f, task.records[:i])
-            f = folder + '/Trial_Number' + "%03d" % (i+1) + '.npy'
-            np.save(f, i)
 
     if debug_simulation:
         print '   Done!'
