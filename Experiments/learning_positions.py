3f1bbfba74b1f39f76baab95a8ea60c508f77e06
mtopalid <meropi.topalidou@inria.fr>
2015-11-26 12:15:30 +0100

    Nothing special

--- Experiments/learning_positions.py
+++ Experiments/learning_positions.py
@@ -13,10 +13,9 @@
 if __name__ == "__main__":
 
     # Include to the path files from cython folder
-    temp = '../cython/'
+    t = '../cython/'
     import sys
-
-    sys.path.append(temp)
+    sys.path.append(t)
     # model file build the structures and initialize the model
     from model import *
     from trial import *
@@ -24,13 +23,13 @@ if __name__ == "__main__":
     import os
     from learning import *
 
-    folder = '../Results/Learn_Positions+M1_learning'
+    folder = '../Results/Learn_Positions_-0_5'#'#+M1_learning
     if not os.path.exists(folder):
         os.makedirs(folder)
 
-    # Initialize the system
+    # # Initialize the system
     task = Task_1ch(n=n_learning_positions_trials)
-
+    #
     # Save the trials
     f = folder + '/Task.npy'
     np.save(f, task.trials)
@@ -39,10 +38,27 @@ if __name__ == "__main__":
     if not os.path.exists(folder2):
         os.makedirs(folder2)
 
+    # f = folder + '/Task.npy'
+    # task.trials = np.load(f)
+    #
+    # f = folder2 + '/Trial_Number1000s.npy'
+    # tr = np.load(f)
+    #
+    # f = folder2 + '/Records1000s.npy'
+    # task.records[:tr] = np.load(f)
+    # # print "History of learning by moves: \n", temp["moves"]
+    # connections["PPC.theta1 -> SMA.theta1"].weights = task.records["Wppc_sma1"][tr-1]
+    # connections["SMA.theta1 -> STR_SMA_PPC.theta1"].weights = task.records["Wsma_str1"][tr-1]
+    # connections["PPC.theta1 -> STR_SMA_PPC.theta1"].weights = task.records["Wppc_str1"][tr-1]
+    # connections["PPC.theta2 -> SMA.theta2"].weights = task.records["Wppc_sma2"][tr-1]
+    # connections["SMA.theta2 -> STR_SMA_PPC.theta2"].weights = task.records["Wsma_str2"][tr-1]
+    # connections["PPC.theta2 -> STR_SMA_PPC.theta2"].weights = task.records["Wppc_str2"][tr-1]
+    # connections["M1.theta1 -> M1.theta2"].weights = task.records["Wm1_1"][tr-1]
+    # connections["M1.theta2 -> M1.theta1"].weights = task.records["Wm1_2"][tr-1]
 
     # Repeated trials with learning after each trial
-    learning_trials_single(task, trials=n_learning_positions_trials, ncues=1, duration=duration_learning_positions,
-                    debugging_arm_learning=False, folder = folder2)
+    learning_trials_continuous(task, trials=n_learning_positions_trials, ncues=1, duration=duration_learning_positions,
+                    debugging_arm_learning=False, folder=folder2, save=True)#, tr=tr-1)
 
     np.set_printoptions(threshold=3)
     P = task.records["best"]
@@ -53,6 +69,3 @@ if __name__ == "__main__":
 
     f = folder + '/Records.npy'
     np.save(f, task.records)
-#12:16 random pos 16sec
-#15:39 81 pos continuous move
-#11:47 81 pos single move
